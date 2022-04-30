fc9190f344b46d7c538c3a3ab5dcfafd588de4bf
Methos <25006979+methos28@users.noreply.github.com>
2022-05-01 00:50:38 +0530

    Add files via upload

---
+++ master_script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo ""
+echo ""
+echo "#############################################################"
+echo "#############################################################"
+echo "##                                                         ##"
+echo "##       Welcome to Nagios Auto Installation Script        ##"
+echo "##               Written By Prasad Dhupkar                 ##"
+echo "##                 Written For Ubuntu                      ##"
+echo "#############################################################"
+echo "#############################################################"
+
+echo ""
+echo "############### Getting Necessary Files ###############"
+sudo wget https://raw.githubusercontent.com/omkardamame/360-degree-monitoring/main/nagios-core.sh
+sudo wget https://raw.githubusercontent.com/omkardamame/360-degree-monitoring/main/nagios-nrpe.sh
+sudo wget https://raw.githubusercontent.com/omkardamame/360-degree-monitoring/main/nagios-plugins.sh
+sudo wget https://raw.githubusercontent.com/omkardamame/360-degree-monitoring/main/client1.cfg
+sudo wget https://raw.githubusercontent.com/omkardamame/360-degree-monitoring/main/client2.cfg
+echo ""
+echo ""
+echo ""
+sudo chmod u+x nagios-core.sh
+sudo chmod u+x nagios-nrpe.sh
+sudo chmod u+x nagios-plugins.sh
+echo ""
+echo ""
+echo ""
+echo "############### Executing Required Scripts ###############"
+echo ""
+echo ""
+
+./nagios-core.sh
+sleep 5
+./nagios-nrpe.sh
+sleep 5
+./nagios-plugins.sh
+echo ""
+echo ""
+echo "############### Execution Successful ! ###############"
+echo ""
+echo ""
